e30e1c8b4c161e178376bd501306f1743affc7c3
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-09-23 21:06:00 +0200

    models associations

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Customer extends Model
+class Invoice extends Model
 {
     use HasFactory;
 
-    public function invoices(){ 
-        return $this->hasMany(Invoice::class);
+    public function cutomer(){ 
+        return $this->belongsTo(Customer::class);
 
     }
 }
